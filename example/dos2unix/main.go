4b38c7ef8a294bc2e552a0980cfe0f03b15634ea
Karrick S. McDermott <karrick@karrick.net>
2018-12-14 04:00:18 -0500

    dos2unix example command reads from files on command line, or stdin

--- example/dos2unix/main.go
+++ example/dos2unix/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"os"
 
 	"github.com/karrick/gocrlf"
+	"github.com/karrick/gorill"
 )
 
 func main() {
 	// implement dos2unix like command
-	_, err := io.Copy(os.Stdout, &gocrlf.LFfromCRLF{Source: os.Stdin})
+
+	var ior io.Reader
+	if flag.NArg() == 0 {
+		ior = os.Stdin
+	} else {
+		ior = &gorill.FilesReader{Pathnames: flag.Args()}
+	}
+
+	_, err := io.Copy(os.Stdout, &gocrlf.LFfromCRLF{Source: ior})
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
 		os.Exit(1)
