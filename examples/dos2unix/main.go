51b2a0f119b2376cb18562566ad8c9e553c884e8
Karrick S. McDermott <kmcdermott@linkedin.com>
2019-07-22 21:50:31 +0000

    removed extra library dependency from example
    I really do not like dependencies from example programs being added.

--- examples/dos2unix/main.go
+++ examples/dos2unix/main.go
@@ -5,26 +5,41 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/karrick/gocrlf"
-	"github.com/karrick/gorill"
 )
+
+var progname = filepath.Base(os.Args[0])
 
 func main() {
 	// implement dos2unix like command
-
 	flag.Parse()
 
 	var ior io.Reader
+	var err error
+
 	if flag.NArg() == 0 {
-		ior = os.Stdin
-	} else {
-		ior = &gorill.FilesReader{Pathnames: flag.Args()}
+		_, err = io.Copy(os.Stdout, &gocrlf.LFfromCRLF{Source: ior})
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", progname, err)
+			os.Exit(1)
+		}
+		os.Exit(0)
 	}
 
-	_, err := io.Copy(os.Stdout, &gocrlf.LFfromCRLF{Source: ior})
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
-		os.Exit(1)
+	for _, arg := range flag.Args() {
+		fh, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", progname, err)
+			continue
+		}
+		_, err = io.Copy(os.Stdout, &gocrlf.LFfromCRLF{Source: ior})
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", progname, err)
+		}
+		if err = fh.Close(); err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", progname, err)
+		}
 	}
 }
